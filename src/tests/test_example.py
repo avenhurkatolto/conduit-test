7bba95e9c1a9e4fc93dc164cd89d3ec4117e4243
DavidZsolt <daavid.zsolt@gmail.com>
2021-08-09 09:32:54 +0200

    chrome options2

--- src/tests/test_example.py
+++ src/tests/test_example.py
@@ -4,7 +4,6 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 import time
@@ -15,10 +14,10 @@ def test_hi():
 
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome()
     driver.get('http://www.google.com/')
     time.sleep(5)  # Let the user actually see something!
     assert driver.current_url == "https://www.google.com/?gws_rd=ssl"
