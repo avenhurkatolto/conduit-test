a282d319800835ef8b42606389a3cce057d648b3
DavidZsolt <daavid.zsolt@gmail.com>
2021-08-09 09:45:59 +0200

    chrome options6

--- src/tests/test_example.py
+++ src/tests/test_example.py
@@ -14,7 +14,7 @@ def test_hi():
 
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    #chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
     driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
